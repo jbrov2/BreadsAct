c95c93992bc3e5213185f641624a35a92b279bf8
zephyr <jafaribrownsha@gmail.com>
2024-01-21 11:30:12 -0500

    code

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+require('dotenv').config()
+const PORT = process.env.PORT
+const app = express()
+
+app.get('/', (req,res)=>{
+    res.send('Welcome to an Awesome App about Breads')
+})
+
+app.use('/breads', require('./controllers/breads_controller'))
+
+app.listen(PORT, ()=> {
+    console.log(`Listening on PORT ${PORT}`)
+})
